fc1216a6d643e82ec781eec77aefe89016df1a46
Opilane TTHK <opilane@tthk.ee>
2024-05-03 12:49:10 +0300

    3maiKenan

---
+++ ForLoop/ForLoopKenan/ForLoopKenan/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ForLoopKenan
+{
+    class ForLoop
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("C# For Loop: Iteration {0}", i);
+            }
+        }
+    }
+}
